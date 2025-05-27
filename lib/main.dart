56f4c5b7a4fe51d3daf3debefc8cb4740e5d95dc
Maximino Peralta <maximino.peralta@freshconsulting.com>
2025-05-26 18:50:15 -0600

    starts with riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:banking_flutter_app/routing/app_router.dart';
 import 'package:banking_flutter_app/presentation/themes/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    const ProviderScope(
+      child: MainApp()
+    )
+  );
 }
 
 class MainApp extends StatelessWidget {
--- lib/presentation/home/screens/counter/counter_screen.dart
+++ lib/presentation/home/screens/counter/counter_screen.dart
@@ -1,26 +1,50 @@
+import 'package:banking_flutter_app/presentation/home/providers/counter_provider.dart';
+import 'package:banking_flutter_app/presentation/home/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterScreen extends StatelessWidget {
+// Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
-  
+
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+            ),
+            onPressed: () {
+              ref
+                  .read(isDarkModeProvider.notifier)
+                  .update((isDarkModeState) => !isDarkModeState);
+            },
+          ),
+        ],
       ),
 
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge),
+        child: Text(
+          'Valor: $clickCounter',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {}
+        onPressed: () {
+          // ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ),
-      
     );
   }
 }
