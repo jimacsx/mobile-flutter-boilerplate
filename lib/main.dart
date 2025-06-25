f5799a9d2fa484e4442850cf0dae88cfa0233af8
Maximino Peralta <maximino.peralta@freshconsulting.com>
2025-06-24 21:43:39 -0600

    Initialize dotenv package to handle env vars

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:banking_flutter_app/presentation/providers/providers.dart';
 import 'package:banking_flutter_app/config/router/app_router.dart';
 import 'package:banking_flutter_app/config/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(
     const ProviderScope(child: MainApp()),
   );
